1096770b1c0ed4c6e8a9c007beb41f0c5934b312
Vova <krosava054@gmail.com>
2025-04-05 04:13:03 +0300

    add producer

--- src/backend/api/routers/shop.py
+++ src/backend/api/routers/shop.py
@@ -1,12 +1,21 @@
 from fastapi import APIRouter #noqa
 from app.service.shop_service import ShopService
 from app.schemas.shop import ShopCreate
+from app.service.kafka_service import KafkaService
+from core.config import TOPIC_PLAY
 
 
 
 shop_router = APIRouter(tags=["Shop"])
 shop_service = ShopService()
+kafka_service = KafkaService()
 
+
+@shop_router.get("/shop/play/{id}")
+def play(id: str):
+    kafka_service.produce(key="get_play", value=id, topic = TOPIC_PLAY)
+    return {"ok": True}
+    
 
 @shop_router.get("/shop")
 def get_shop():
@@ -23,3 +32,4 @@ def delete_shop(id: str):
 @shop_router.get("/shop/{id}")
 def get_shop_by_id(id: str):
     return shop_service.get_by_id(id)
+
