48d9b217145bac01a4713bf1ab94c08ef9d8c299
Alexander <dctrback@bk.ru>
2025-06-23 23:45:49 +0300

    change domain:

--- src/frontend/src/config/api.ts
+++ src/frontend/src/config/api.ts
@@ -1,2 +1,2 @@
-export const API_SERVER_URL = 'https://api.labofdev.ru/v1';
-export const NOTYFIER_SERVER_URL = 'https://notify.labofdev.ru';
+export const API_SERVER_URL = 'https://api.sidodgi.tech/v1';
+export const NOTYFIER_SERVER_URL = 'https://notify.sidodgi.tech';
